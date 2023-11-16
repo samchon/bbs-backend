5c95b8ee7ffa6990db7fcaf78a1c31bd60535dd8
Jeongho Nam <samchon.github@gmail.com>
2023-11-17 02:06:10 +0900

    Add `webpack.config.js` and `postgres.sh` files

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  // CUSTOMIZE HERE
+  entry: {
+    server: "./src/executable/server.ts",
+    "updator-master": "./src/executable/updator-master.ts",
+    "updator-slave": "./src/executable/updator-slave.ts",
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  optimization: {
+    minimize: true,
+  },
+
+  // JUST KEEP THEM
+  externals: [nodeExternals()],
+  mode: "production",
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
