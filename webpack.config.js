72c4d5478239f27ef9d36f3072bd40d875847748
Jeongho Nam <samchon.github@gmail.com>
2023-11-17 16:59:39 +0900

    Change webpack config.
    Changed webpack config to build only one single JS file that which even does not require the `node_modules` directory.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
 const path = require("path");
-const nodeExternals = require("webpack-node-externals");
 
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const WriteFilePlugin = require("write-file-webpack-plugin");
+const { IgnorePlugin } = require("webpack");
+
+const lazyImports = [
+  "@fastify/static",
+  "@fastify/view",
+  "@nestjs/microservices",
+  "@nestjs/websockets",
+  "class-transformer",
+  "class-validator",
+];
+
+// @reference https://tech-blog.s-yoshiki.com/entry/297
 module.exports = {
   // CUSTOMIZE HERE
   entry: {
@@ -17,7 +30,6 @@ module.exports = {
   },
 
   // JUST KEEP THEM
-  externals: [nodeExternals()],
   mode: "production",
   target: "node",
   module: {
@@ -32,4 +44,31 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: ".env",
+          to: "[name][ext]",
+        },
+        {
+          from: "./node_modules/.prisma/client/*.node",
+          to: () => Promise.resolve("[path][name][ext]"),
+        },
+      ],
+    }),
+    new WriteFilePlugin(),
+    new IgnorePlugin({
+      checkResource: (resource) => {
+        if (lazyImports.some((modulo) => resource.startsWith(modulo))) {
+          try {
+            require.resolve(resource);
+          } catch (err) {
+            return true;
+          }
+        }
+        return false;
+      },
+    }),
+  ],
 };
