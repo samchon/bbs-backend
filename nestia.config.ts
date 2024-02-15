d14069c6f7642d462f3683173e9d9386a960de4b
Jeongho Nam <samchon.github@gmail.com>
2024-02-16 00:30:05 +0900

    Upgrade nestia dependencies.
    For one month, `typia` and `@nestia/sdk` had been changed a lot, many new features and fixing trivial bugs.
    
    This PR updates those `typia` and `nestia` dependencies, so that accept new features and also avoid potential bugs.

--- nestia.config.ts
+++ nestia.config.ts
@@ -1,10 +1,11 @@
 import { INestiaConfig } from "@nestia/sdk";
 import { NestFactory } from "@nestjs/core";
+import { FastifyAdapter } from "@nestjs/platform-fastify";
 
 import { BbsModule } from "./src/BbsModule";
 
 export const NESTIA_CONFIG: INestiaConfig = {
-  input: () => NestFactory.create(BbsModule),
+  input: () => NestFactory.create(BbsModule, new FastifyAdapter()),
   output: "src/api",
   swagger: {
     output: "packages/api/swagger.json",
@@ -16,6 +17,7 @@ export const NESTIA_CONFIG: INestiaConfig = {
     ],
     beautify: true,
   },
+  primitive: false,
   simulate: true,
 };
 export default NESTIA_CONFIG;
